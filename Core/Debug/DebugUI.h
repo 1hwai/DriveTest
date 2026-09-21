108a93e0d38ff8774921c843574bac5e46a80789
Hawon <73703748+1hwai@users.noreply.github.com>
2026-09-21 18:11:47 +0900

    Phase 4-2: add hierarchy UI

--- Core/Debug/DebugUI.h
+++ Core/Debug/DebugUI.h
@@ -3,6 +3,7 @@
 #include <SDL3/SDL.h>
 
 class SimulationController;
+class World;
 
 class DebugUI {
 public:
@@ -11,7 +12,8 @@ public:
     bool Initialize(
         SDL_Window* window,
         SDL_GLContext context,
-        SimulationController& simulation
+        SimulationController& simulation,
+        World& world
     );
 
     void ProcessEvent(const SDL_Event& event);
@@ -20,6 +22,10 @@ public:
 
 private:
     SimulationController* m_simulation;
+    World* m_world;
+
     bool m_initialized;
     bool m_showSimulationWindow;
+    bool m_showHierarchyWindow;
+    int m_selectedObjectIndex;
 };
