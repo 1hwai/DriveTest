d645663cbf978a29824c9c3e0f4c04a68ae9097f
Hawon <73703748+1hwai@users.noreply.github.com>
2026-09-23 00:08:10 +0900

    Implement basic lit mesh rendering

--- Rendering/Shader.h
+++ Rendering/Shader.h
@@ -1,7 +1,11 @@
 #pragma once
 
 #include <string>
+#include <unordered_map>
+
+#include "../Core/Math/Mat3.h"
 #include "../Core/Math/Mat4.h"
+#include "../Core/Math/Vec3.h"
 
 class Shader {
 public:
@@ -13,7 +17,12 @@ public:
     void Destroy();
 
     void SetMat4(const char* name, const Mat4& matrix);
+    void SetMat3(const char* name, const Mat3& matrix);
+    void SetVec3(const char* name, const Vec3& vector);
 
 private:
+    int GetUniformLocation(const char* name);
+
     unsigned int m_program;
+    std::unordered_map<std::string, int> m_uniformLocations;
 };
