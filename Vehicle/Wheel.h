a02a70aa05408a3c4900d8f6d34c9d2667d010b4
Hawon <73703748+1hwai@users.noreply.github.com>
2026-09-23 01:21:15 +0900

    Implement wheel state for suspension test

---
+++ Vehicle/Wheel.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "../Core/Math/Vec3.h"
+
+class PhysicsWorld;
+class RigidBody;
+class Suspension;
+
+class Wheel {
+public:
+    Wheel();
+
+    void SetLocalPosition(const Vec3& position);
+    const Vec3& GetLocalPosition() const;
+
+    void SetRadius(float radius);
+    float GetRadius() const;
+
+    void Update(
+        RigidBody& body,
+        PhysicsWorld& physicsWorld,
+        const Suspension& suspension,
+        float deltaTime
+    );
+
+    bool IsGrounded() const;
+    float GetSuspensionLength() const;
+    float GetCompression() const;
+    float GetForce() const;
+    const Vec3& GetWorldPosition() const;
+    const Vec3& GetContactPoint() const;
+
+private:
+    Vec3 m_localPosition;
+    float m_radius;
+
+    bool m_grounded;
+    float m_suspensionLength;
+    float m_compression;
+    float m_force;
+
+    Vec3 m_worldPosition;
+    Vec3 m_contactPoint;
+};
