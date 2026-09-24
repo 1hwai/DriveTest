d65dce8eb722471e0be424a356ae3cc2d1f61b25
Hawon <73703748+1hwai@users.noreply.github.com>
2026-09-24 23:55:30 +0900

    Refine tire slip and friction model

--- Vehicle/Tire.h
+++ Vehicle/Tire.h
@@ -13,11 +13,13 @@ public:
     void SetDynamicFriction(float friction);
     void SetLongitudinalStiffness(float stiffness);
     void SetLateralStiffness(float stiffness);
+    void SetRollingResistance(float coefficient);
 
     float GetStaticFriction() const;
     float GetDynamicFriction() const;
     float GetLongitudinalStiffness() const;
     float GetLateralStiffness() const;
+    float GetRollingResistance() const;
 
     Vec3 CalculateForce(
         const RigidBody& body,
@@ -29,4 +31,5 @@ private:
     float m_dynamicFriction;
     float m_longitudinalStiffness;
     float m_lateralStiffness;
+    float m_rollingResistance;
 };
